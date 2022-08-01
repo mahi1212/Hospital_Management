51d65a9ee8a193526f429fcf674bb51f561fb264
unknown <@gmail.com>
2022-08-01 23:42:49 +0600

    add patients table

--- src/Pages/Patients/Patients.js
+++ src/Pages/Patients/Patients.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import * as React from 'react';
+import './Patients.css'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const Patients = () => {
-  return (
-    <div>Patients</div>
-  )
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default Patients
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, "29-2-2021", 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Patients() {
+  return (
+
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: '700', margin:'0 auto' }} aria-label="customized table" className='patientTable'>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="right">Patients Name </StyledTableCell>
+            <StyledTableCell align="center">Doctors Name</StyledTableCell>
+            <StyledTableCell align="center">Contact</StyledTableCell>
+            <StyledTableCell align="center">Date&nbsp;</StyledTableCell>
+            <StyledTableCell align="center">Time&nbsp;</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell align="right" component="th" scope="row">{row.name}</StyledTableCell>
+              <StyledTableCell align="center">{row.calories}</StyledTableCell>
+              <StyledTableCell align="center">{row.fat}</StyledTableCell>
+              <StyledTableCell align="center">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="center">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    
+  );
+}
