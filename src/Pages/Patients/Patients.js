517b8d09f2882b8c4525f07c5f5ec87943ae2b8c
unknown <@gmail.com>
2022-08-02 01:11:43 +0600

    added patients table

--- src/Pages/Patients/Patients.js
+++ src/Pages/Patients/Patients.js
@@ -34,7 +34,7 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, "29-2-2021", 4.0),
+  createData('Frozen yoghurt', 159, 6.0, "  ", 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
