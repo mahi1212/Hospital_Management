977870b50dda2ee5aae9deb1441318a6a7b71c4f
mahi1212 <mahi121.mr@gmail.com>
2022-08-12 03:51:44 +0600

    added feedback form by Mahinur Rahman

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,18 +1,28 @@
+import { Grid } from '@mui/material'
 import React from 'react'
-import Map from '../../Map/Map'
+// import Map from '../../Map/Map'
 import Banner from '../Banner/Banner'
 import DetailsChart from '../DetailsChart/DetailsChart'
+import Feedback from '../Feedback/Feedback'
 import TodaysDoctors from '../TodaysDoctors/TodaysDoctors'
 
 const Home = () => {
   return (
     <div>
-        <Banner></Banner>
-        <DetailsChart></DetailsChart>
-        <TodaysDoctors></TodaysDoctors>
-        {/* <Map></Map> */}
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <Banner></Banner>
+          <DetailsChart></DetailsChart>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Feedback></Feedback>
+        </Grid>
+      </Grid>
+      <TodaysDoctors></TodaysDoctors>
+      {/* <Map></Map> */}
     </div>
   )
 }
 
 export default Home
+
