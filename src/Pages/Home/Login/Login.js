30e4470cd79faab88a165f2aa088ecd28c3f72eb
mahi1212 <mahi121.mr@gmail.com>
2022-08-18 13:38:01 +0600

    added signup-login page by Jenny

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -3,76 +3,68 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Button, TextField, Typography } from '@mui/material';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
 
 const Login = () => {
-    const [loginData,setLoginData] = useState({})
-    const handleOnChange = e => {
-      const feild = e.target.name;
-      const value = e.target.value;
-      console.log(feild, value)
-      const newLoginData = { ...loginData }
-      newLoginData[feild] = value;
-      setLoginData(newLoginData);
-  }
-  
-    const handleLoginSubmit = e => {
-      // alert('submit')
-      e.preventDefault();
+  const [loginData, setLoginData] = useState({})
+  const handleOnChange = e => {
+    const feild = e.target.name;
+    const value = e.target.value;
+    console.log(feild, value)
+    const newLoginData = { ...loginData }
+    newLoginData[feild] = value;
+    setLoginData(newLoginData);
   }
 
-
-
-
-
-
-
-
+  const handleLoginSubmit = e => {
+    // alert('submit')
+    e.preventDefault();
+  }
 
   return (
     <Container fixed>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} >
-        <Typography variant="bosy1" gutterBottom component="div">
-                                Login
-          </Typography>
-          <form onClick={handleLoginSubmit}>
-          <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your mail"
-              name='email'
-              onChange={handleOnChange}
-              variant="standard" />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} >
+            <Typography variant="bosy1" gutterBottom component="div">
+              Login
+            </Typography>
+            <form onClick={handleLoginSubmit}>
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your mail"
+                name='email'
+                onChange={handleOnChange}
+                variant="standard" />
 
-            <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your password"
-              name='password'
-              onChange={handleOnChange}
-              variant="standard"
-              type="password"  
-            />  
-             <Button
-            sx={{ width: '50%', m: 1 }}
-            varient="contained"
-            type="submit"
-            style={{
-              backgroundColor: '#2DE1F0',
-              color: 'white'
-            }}>Login</Button>
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your password"
+                name='password'
+                onChange={handleOnChange}
+                variant="standard"
+                type="password"
+              />
+              <Button
+                sx={{ width: '50%', m: 1 }}
+                varient="contained"
+                type="submit"
+                style={{
+                  backgroundColor: '#2DE1F0',
+                  color: 'white'
+                }}>Login</Button>
 
-       <Link to='/registration'><Button  sx={{ width: '50%', m: 1 }} color="inherit">For the First time</Button></Link>
-          </form>
+              <Link to='/registration'><Button sx={{ width: '50%', m: 1 }} color="inherit">For the First time</Button></Link>
+            </form>
+          </Grid>
+
         </Grid>
-      
-      </Grid>
-    </Box>
+      </Box>
     </Container>
   )
 }
--- src/Pages/Home/Registration/Registration.js
+++ src/Pages/Home/Registration/Registration.js
@@ -3,117 +3,109 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Button, TextField, Typography } from '@mui/material';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Registration = () => {
 
 
 
-    const [loginData,setLoginData] = useState({})
-    const handleOnChange = e => {
-      const feild = e.target.name;
-      const value = e.target.value;
-      console.log(feild, value)
-      const newLoginData = { ...loginData }
-      newLoginData[feild] = value;
-      setLoginData(newLoginData);
-  }
-  
-    const handleLoginSubmit = e => {
-      // alert('submit')
-      e.preventDefault();
+  const [loginData, setLoginData] = useState({})
+  const handleOnChange = e => {
+    const feild = e.target.name;
+    const value = e.target.value;
+    console.log(feild, value)
+    const newLoginData = { ...loginData }
+    newLoginData[feild] = value;
+    setLoginData(newLoginData);
   }
 
-
-
-
-
-
-
-
+  const handleLoginSubmit = e => {
+    // alert('submit')
+    e.preventDefault();
+  }
 
   return (
-<Container fixed>
-<Typography variant="bosy1" gutterBottom component="div">
-         Register as patient
-          </Typography>
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        
-        <Grid item xs={12} md={6} >
-        
-          <form onClick={handleLoginSubmit}>
-          <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="First Name"
-              name='text'
-              onChange={handleOnChange}
-              variant="standard" />
-          <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your mail"
-              name='email'
-              onChange={handleOnChange}
-              variant="standard" />
+    <Container fixed>
+      <Typography variant="bosy1" gutterBottom component="div">
+        Register as patient
+      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
 
-            <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your password"
-              name='password'
-              onChange={handleOnChange}
-              variant="standard"
-              type="password"  
-            />  
-        
-         
-          </form>
-        </Grid>
-        <Grid item xs={12} md={6} >
-        <form onClick={handleLoginSubmit}>
-             <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Last Name"
-              name='text'
-              onChange={handleOnChange}
-              variant="standard" />
+          <Grid item xs={12} md={6} >
+
+            <form onClick={handleLoginSubmit}>
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="First Name"
+                name='text'
+                onChange={handleOnChange}
+                variant="standard" />
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your mail"
+                name='email'
+                onChange={handleOnChange}
+                variant="standard" />
 
               <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your phone"
-              name='email'
-              onChange={handleOnChange}
-              variant="standard" />
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your password"
+                name='password'
+                onChange={handleOnChange}
+                variant="standard"
+                type="password"
+              />
+
+
+            </form>
+          </Grid>
+          <Grid item xs={12} md={6} >
+            <form onClick={handleLoginSubmit}>
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Last Name"
+                name='text'
+                onChange={handleOnChange}
+                variant="standard" />
 
               <TextField
-              sx={{width:'50%',m:1}}
-              id="standard-basic"
-              label="Your password"
-              name='password'
-              onChange={handleOnChange}
-              variant="standard"
-              type="password"  
-            /> 
-                 <Button
-            sx={{ width: '50%', m: 3 }}
-            varient="contained"
-            type="submit"
-            style={{
-              backgroundColor: '#2DE1F0',
-              color: 'white'
-            }}>Register</Button>
-                  
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your phone"
+                name='email'
+                onChange={handleOnChange}
+                variant="standard" />
 
-        </form>
+              <TextField
+                sx={{ width: '50%', m: 1 }}
+                id="standard-basic"
+                label="Your password"
+                name='password'
+                onChange={handleOnChange}
+                variant="standard"
+                type="password"
+              />
+              <Button
+                sx={{ width: '50%', m: 3 }}
+                varient="contained"
+                type="submit"
+                style={{
+                  backgroundColor: '#2DE1F0',
+                  color: 'white'
+                }}>Register</Button>
+
+
+            </form>
+          </Grid>
+
         </Grid>
-      
-      </Grid>
-    </Box>
-    <Link to='/login'><Button  sx={{ width: '50%', m: 1 }} color="inherit">Already have an account</Button></Link>
+      </Box>
+      <Link to='/login'><Button sx={{ width: '50%', m: 1 }} color="inherit">Already have an account</Button></Link>
     </Container>
   )
 }
