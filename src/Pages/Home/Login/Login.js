d6b96d09ab3d87a0b0980084f87e708d394d9b42
mahi1212 <mahi121.mr@gmail.com>
2022-08-24 21:34:57 +0600

    designed login

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,72 +1,88 @@
-import React, { useState } from 'react'
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { Button, TextField, Typography } from '@mui/material';
-import { Link } from 'react-router-dom'
-
-
-
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Button, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [loginData, setLoginData] = useState({})
-  const handleOnChange = e => {
+  const [loginData, setLoginData] = useState({});
+  const handleOnChange = (e) => {
     const feild = e.target.name;
     const value = e.target.value;
-    console.log(feild, value)
-    const newLoginData = { ...loginData }
+    console.log(feild, value);
+    const newLoginData = { ...loginData };
     newLoginData[feild] = value;
     setLoginData(newLoginData);
-  }
+  };
 
-  const handleLoginSubmit = e => {
+  const handleLoginSubmit = (e) => {
     // alert('submit')
     e.preventDefault();
-  }
+  };
 
   return (
     <Container fixed>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={12} >
+      <Box
+        sx={{
+          flexGrow: 1,
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{ background: "#fff", padding: "6rem 0rem" }}
+        >
+          <Grid item xs={12} md={12}>
             <Typography variant="bosy1" gutterBottom component="div">
               Login
             </Typography>
             <form onClick={handleLoginSubmit}>
               <TextField
-                sx={{ width: '50%', m: 1 }}
+                sx={{ width: { sm: "none", md: "50%", lg: "50%" }, m: 1 }}
                 id="standard-basic"
                 label="Your mail"
-                name='email'
+                name="email"
                 onChange={handleOnChange}
-                variant="standard" />
+                variant="standard"
+              />
 
               <TextField
-                sx={{ width: '50%', m: 1 }}
+                sx={{ width: { sm: "none", md: "50%", lg: "50%" }, m: 1 }}
                 id="standard-basic"
                 label="Your password"
-                name='password'
+                name="password"
                 onChange={handleOnChange}
                 variant="standard"
                 type="password"
               />
               <Button
-                sx={{ width: '50%', m: 1 }}
+                sx={{ width: "50%", m: 1 }}
                 varient="contained"
                 type="submit"
                 style={{
-                  backgroundColor: '#2DE1F0',
-                  color: 'white'
-                }}>Login</Button>
+                  backgroundColor: "#2DE1F0",
+                  color: "white",
+                }}
+              >
+                Login
+              </Button>
 
-              <Link to='/registration'><Button sx={{ width: '50%', m: 1 }} color="inherit">For the First time</Button></Link>
+              <Link to="/registration">
+                <Button sx={{ width: "50%", m: 1 }} color="inherit">
+                  For the First time?
+                </Button>
+              </Link>
             </form>
           </Grid>
-
         </Grid>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
